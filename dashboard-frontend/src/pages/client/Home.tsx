e63fcd019ac47c5fe8471b4cb11ece788b0458ca
Leo Zhang <hzha0986@uni.sydney.edu.au>
2025-09-25 01:00:02 +1000

    Adjusted orders of cards

--- dashboard-frontend/src/pages/client/Home.tsx
+++ dashboard-frontend/src/pages/client/Home.tsx
@@ -7,39 +7,37 @@ import BmiCard from "../../components/DashboardCardContent/Bmi/BmiCard.tsx"
 import FatMuscleWaterCard from "../../components/DashboardCardContent/Body/FatMuscleWaterCard.tsx";
 import WorkoutOverview from '../../components/DashboardCardContent/WorkoutOverview/WorkoutOverview.tsx';
 const Home = () => {
-    return(
-        <div className='w-full h-full py-4 grid grid-cols-1 auto-rows-[calc((100%-var(--spacing)*8)/3)] md:grid-cols-6 lg:grid-cols-12 gap-4 col-span-2 row-span-3 overflow-y-scroll pl-2 pr-4'>
-            <DashboardCard id='avg daily intake' className='text-4xl font-light md:col-span-2 lg:col-span-4'>
-                <FoodIntakeWrapper/>
+    return (
+        <div className='w-full h-full py-4 grid grid-cols-1 auto-rows-[calc((100%-var(--spacing)*8)/3)] md:grid-cols-6 lg:grid-cols-15 gap-4 col-span-2 row-span-3 overflow-y-scroll pl-2 pr-4'>
+            <DashboardCard id='workout' className='md:col-span-2 lg:col-span-12'>
+                <WorkoutOverview />
             </DashboardCard>
 
-            <DashboardCard id='percentages' className='md:col-span-4 lg:col-span-4'>
+            <DashboardCard id='Basic' className=' md:col-span-2 lg:col-span-3'>
+                <BmiCard showLabels={true} />
+            </DashboardCard>
+
+            <DashboardCard id='avg daily intake' className='text-4xl font-light md:col-span-2 lg:col-span-5'>
+                <FoodIntakeWrapper />
+            </DashboardCard>
+
+            <DashboardCard id='percentages' className='md:col-span-4 lg:col-span-5'>
                 <FatMuscleWaterCard />
-            </DashboardCard>
-
-            <DashboardCard id='Basic' className=' md:col-span-2 lg:col-span-4'>
-                <BmiCard showLabels={true}/>
             </DashboardCard>
 
             <DashboardCard id='wlgr wlgx' className='md:col-span-2 lg:col-span-5'>
                 <WlgrWlgxCards />
             </DashboardCard>
 
-            <DashboardCard id='sleeping duration' className='md:col-span-2 lg:col-span-3'>
+            <DashboardCard id='sleeping duration' className='md:col-span-2 lg:col-span-4'>
                 <SleepGauges />
             </DashboardCard>
 
-            <DashboardCard id='lsmz' className='md:col-span-2 lg:col-span-6'>
+            <DashboardCard id='lsmz' className='md:col-span-2 lg:col-span-11'>
                 <WeightMetrics />
             </DashboardCard>
 
-            <DashboardCard id='workout' className='md:col-span-2 lg:col-span-6'>
-                <WorkoutOverview />
-            </DashboardCard>
 
-            <DashboardCard>
-
-            </DashboardCard>
         </div>
     )
 }
