905e499e036d0dbe3508ffa5735847bb69b8ee05
hwei0665 <hwei0665@uni.sydney.edu.au>
2025-10-15 23:47:58 +1100

    updatad dietary recommendation logic and UI to show recommended intake based on selected diet goal added DietRecommendationService to handle dietary recommendation logic updated FoodIntakeController to fetch recommended intake data updated FoodIntakeWrapper and FoodBarGraph to display recommended intake data

--- dashboard-server/src/main/java/com/cs79_1/interactive_dashboard/DTO/DietaryIntake/FoodIntakeByCategory.java
+++ dashboard-server/src/main/java/com/cs79_1/interactive_dashboard/DTO/DietaryIntake/FoodIntakeByCategory.java
@@ -4,14 +4,20 @@ public class FoodIntakeByCategory {
     
     private String group;
     private double actual;
+    private double recommended;
 
-    public FoodIntakeByCategory(String group, double actual) {
+    public FoodIntakeByCategory(String group, double actual, double recommended) {
         this.group = group;
         this.actual = actual;
+        this.recommended = recommended;
     }
 
     public String getGroup() {
         return group;
+    }
+
+    public double getRecommended() {
+        return recommended;
     }
 
     public double getActual() {
