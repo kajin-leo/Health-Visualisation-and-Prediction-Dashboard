5a60189d09868315bbba11979275ff10d2deb1bd
Leo Zhang <hzha0986@uni.sydney.edu.au>
2025-10-21 15:05:11 +1100

    Dark Mode for Simulate section

--- dashboard-frontend/src/components/Simulation/SimulateActivity/SimulateActivity.tsx
+++ dashboard-frontend/src/components/Simulation/SimulateActivity/SimulateActivity.tsx
@@ -165,9 +165,9 @@ const SimulateActivity = ({ MockData, isWeekend }: { MockData?: ActivityData, is
     }
 
     return (
-        <div className="w-full h-full flex flex-col gap-2 p-5">
+        <div className="w-full h-full flex flex-col gap-2 p-2">
             <div className="w-fit h-fit flex flex-shrink-0 flex-col">
-                <h1 className="w-fit opacity-100 rounded-lg text-gray-800 pl-1 text-lg tracking-tight font-bold font-[Nunito] flex-shrink-0">
+                <h1 className="w-fit opacity-100 rounded-lg text-gray-800 dark:text-gray-200 select-none pl-1 text-lg tracking-tight font-bold font-[Nunito] flex-shrink-0">
                     Activity Simulation
                 </h1>
                 <h2 className="text-sm px-1 font-serif">
@@ -180,11 +180,11 @@ const SimulateActivity = ({ MockData, isWeekend }: { MockData?: ActivityData, is
                 </div>
                 <div id="panel" className="flex-shrink-0 p-5 flex flex-col items-center gap-5 select-none">
                     <div className="flex gap-2">
-                        <div className={`w-25 h-10 transition-all flex flex-col items-center justify-center rounded-full bg-lime-300 p-1 border-3 border-white/50 text-lime-700 ${(canSimulate && !isSimulating) ? ' hover:border-2 hover:shadow-md active:border-2 active:border-lime-400/20 active:inset-shadow-sm/40 active:shadow-none active:text-shadow-lime-800/20 active:text-shadow-xs' : 'cursor-not-allowed bg-lime-400/20'}`}
+                        <div className={`w-25 h-10 transition-all flex flex-col items-center justify-center rounded-full bg-lime-300 dark:bg-lime-800 p-1 border-3 border-white/50 dark:border-lime-950/90 text-lime-700 ${(canSimulate && !isSimulating) ? 'dark:text-lime-300 hover:border-2 hover:shadow-md active:border-2 active:border-lime-400/20 active:inset-shadow-sm/40 active:shadow-none active:text-shadow-lime-800/20 active:text-shadow-xs' : 'cursor-not-allowed bg-lime-400/20 dark:bg-lime-800/60 dark:text-lime-100/20'}`}
                             onClick={submitSimulation}>
                             Simulate
                         </div>
-                        <div className="w-25 h-10 transition-all flex flex-col items-center justify-center rounded-full bg-red-300 p-1 border-3 border-white/50 text-red-700 hover:border-2 hover:shadow-md active:border-2 active:border-red-400/20 active:inset-shadow-sm/40 active:shadow-none active:text-shadow-red-800/20 active:text-shadow-xs"
+                        <div className="w-25 h-10 transition-all flex flex-col items-center justify-center rounded-full bg-red-300 dark:bg-red-800 p-1 border-3 border-white/50 dark:border-red-950/90 text-red-700 dark:text-red-300 hover:border-2 hover:shadow-md active:border-2 active:border-red-400/20 active:inset-shadow-sm/40 active:shadow-none active:text-shadow-red-800/20 active:text-shadow-xs"
                             onClick={resetSimulation}>
                             <h1>Reset</h1>
                         </div>
--- dashboard-frontend/src/pages/client/Simulation.tsx
+++ dashboard-frontend/src/pages/client/Simulation.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import HeatmapChart from "../../components/Simulation/HealthStatusHeatmap/HealthStatus";
 import SimulateActivity from "../../components/Simulation/SimulateActivity/SimulateActivity";
 import HealthStatusHeatmap from "../../components/Simulation/HealthStatusHeatmap/HealthStatusHeatmap";
+import DashboardCard from "../../components/DashboardCard";
 
 const SimulationView = () => {
     const [group, setGroup] = useState<"weekdays" | "weekends">("weekdays");
@@ -12,8 +13,8 @@ const SimulationView = () => {
                 <div className="flex gap-2 mt-2">
                     <button
                         className={`px-3 py-1 rounded-lg shadow-md text-sm ${group === "weekdays"
-                            ? "bg-gradient-to-r from-blue-400 to-blue-300 text-white"
-                            : "bg-gray-100"
+                            ? "bg-gradient-to-r from-blue-400 to-blue-300 dark:from-purple-700 dark:to-purple-500 text-white"
+                            : "bg-gray-100 dark:bg-gray-900/40"
                             }`}
                         onClick={() => setGroup("weekdays")}
                     >
@@ -21,8 +22,8 @@ const SimulationView = () => {
                     </button>
                     <button
                         className={`px-3 py-1 rounded-lg shadow-md text-sm ${group === "weekends"
-                            ? "bg-gradient-to-r from-blue-400 to-blue-300 text-white"
-                            : "bg-gray-100"
+                            ? "bg-gradient-to-r from-blue-400 to-blue-300 dark:from-purple-700 dark:to-purple-500 text-white"
+                            : "bg-gray-100 dark:bg-gray-900/40"
                             }`}
                         onClick={() => setGroup("weekends")}
                     >
@@ -32,17 +33,19 @@ const SimulationView = () => {
             </div>
 
             <div className="flex-1 flex flex-col pl-2 pr-4 gap-4">
-                <div className="w-full flex-1 min-h-0 bg-white/60 rounded-2xl shadow-md/20 shadow-blue-500 outline-1 outline-white/100 ">
+                <DashboardCard className="w-full flex-1 min-h-0" noHover={true}>
                     <SimulateActivity isWeekend={group === "weekends"} />
-                </div>
+                </DashboardCard>
 
 
-                <div className="bg-white/60 flex-shrink-0 rounded-2xl shadow-md/20 shadow-blue-500 outline-1 outline-white/100 w-full flex flex-col p-4">
-                    <h1 className="opacity-100 rounded-lg text-gray-800 pl-1 text-lg tracking-tight font-bold font-[Nunito]">
-                        Heatmap
-                    </h1>
-                    <HealthStatusHeatmap isWeekend={group === "weekends"} />
-                </div>
+                <DashboardCard noHover={true}>
+                    <div className="w-full h-full p-2">
+                        <h1 className="opacity-100 rounded-lg text-gray-800 dark:text-gray-200 select-none pl-1 text-lg tracking-tight font-bold font-[Nunito]">
+                            Heatmap
+                        </h1>
+                        <HealthStatusHeatmap isWeekend={group === "weekends"} />
+                    </div>
+                </DashboardCard>
             </div>
 
 
