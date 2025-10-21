54673f85b13be3ff22f6d9124822b32d7a40ca1f
Leo Zhang <hzha0986@uni.sydney.edu.au>
2025-10-21 15:02:30 +1100

    Dark Mode for Body Composition Card

--- dashboard-frontend/src/components/DashboardCardContent/Body/FatMuscleWaterCard.tsx
+++ dashboard-frontend/src/components/DashboardCardContent/Body/FatMuscleWaterCard.tsx
@@ -58,17 +58,17 @@ export default function FatMuscleWaterCard() {
 
     return (
         <div className="rounded-xl w-full h-full flex flex-col pb-5">
-            <h1 className="w-fit opacity-100 rounded-lg text-gray-800 pl-1 text-lg tracking-tight font-bold font-[Nunito] flex-shrink-0">
+            <h1 className="w-fit opacity-100 rounded-lg text-gray-800 dark:text-gray-200 pl-1 text-lg tracking-tight font-bold font-[Nunito] flex-shrink-0 select-none">
                 Body Composition
             </h1>
             <div className="flex gap-2 w-full h-full items-center justify-between p-4 flex-1">
                 <div className=" min-w-0 h-full min-h-0 drop-shadow-lg">
-                    <Pie data={chartData} options={options} />
+                    <Pie data={chartData} options={options} className="dark:brightness-70 dark:saturate-100 dark:contrast-150"/>
                 </div>
                 <div className="flex flex-col gap-2 flex-shrink-0 pr-5">
                     {chartData.labels.map((label, index) => (
                         <div key={index} className="flex gap-2 items-center">
-                            <div className="w-5 h-3 rounded-full border-1 border-black/20 shadow-md" style={{background: chartData.datasets[0].backgroundColor[index]}} />
+                            <div className="w-5 h-3 rounded-full border-1 border-black/20 shadow-md dark:brightness-70 dark:saturate-100 dark:contrast-150" style={{background: chartData.datasets[0].backgroundColor[index]}} />
                             <div>
                                 <h1 className="text-md font-semibold">{label}</h1>
                                 <h2 className="text-sm">{chartData.datasets[0].data[index].toFixed(2)}%</h2>
